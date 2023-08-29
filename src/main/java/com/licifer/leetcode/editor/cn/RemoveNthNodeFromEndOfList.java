675db3751cdfadd44459a3a04bcd7961b8ca7feb
李思 <315068263@qq.com>
2023-08-29 21:29:11 +0800

    删除倒数第K个元素

---
+++ src/main/java/com/licifer/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,113 @@
+  /**
+<p>给你一个链表，删除链表的倒数第&nbsp;<code>n</code><em>&nbsp;</em>个结点，并且返回链表的头结点。</p>
+
+<p>&nbsp;</p>
+
+<p><strong>示例 1：</strong></p> 
+<img alt="" src="https://assets.leetcode.com/uploads/2020/10/03/remove_ex1.jpg" style="width: 542px; height: 222px;" /> 
+<pre>
+<strong>输入：</strong>head = [1,2,3,4,5], n = 2
+<strong>输出：</strong>[1,2,3,5]
+</pre>
+
+<p><strong>示例 2：</strong></p>
+
+<pre>
+<strong>输入：</strong>head = [1], n = 1
+<strong>输出：</strong>[]
+</pre>
+
+<p><strong>示例 3：</strong></p>
+
+<pre>
+<strong>输入：</strong>head = [1,2], n = 1
+<strong>输出：</strong>[1]
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示：</strong></p>
+
+<ul> 
+ <li>链表中结点的数目为 <code>sz</code></li> 
+ <li><code>1 &lt;= sz &lt;= 30</code></li> 
+ <li><code>0 &lt;= Node.val &lt;= 100</code></li> 
+ <li><code>1 &lt;= n &lt;= sz</code></li> 
+</ul>
+
+<p>&nbsp;</p>
+
+<p><strong>进阶：</strong>你能尝试使用一趟扫描实现吗？</p>
+
+<details><summary><strong>Related Topics</strong></summary>链表 | 双指针</details><br>
+
+<div>👍 2650, 👎 0<span style='float: right;'><span style='color: gray;'><a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a> | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a> | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a></span></span></div>
+
+
+
+
+*/
+
+
+  /**
+   * 解题思路：
+   * 1.构造一个dummy新链表，不然删除链表最后一个元素时，会报空指针，重点。
+   * 2.要删除链表中的一个元素必须找到它前面的一个元素
+   **/
+
+
+  package com.licifer.leetcode.editor.cn;
+  public class RemoveNthNodeFromEndOfList{
+      public static void main(String[] args) {
+           Solution solution = new RemoveNthNodeFromEndOfList().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode p1 = dummy;
+        ListNode p2 = dummy;
+
+        for (int i = 0; i < n + 1; i++) {
+            p1 = p1.next;
+        }
+        while (p1 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        ListNode deleteNode = p2.next;
+        ListNode next = deleteNode.next;
+        p2.next = next;
+        deleteNode.next = null;
+        return dummy.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+  }
