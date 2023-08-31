f208e9fed2703517ebb59edce0ea5bcd0a3892fb
李思 <315068263@qq.com>
2023-08-31 21:59:59 +0800

    环形链表

--- src/main/java/com/licifer/leetcode/editor/cn/LinkedListCycleIi.java
+++ src/main/java/com/licifer/leetcode/editor/cn/LinkedListCycleIi.java
@@ -1,90 +1,138 @@
-  /**
-<p>给定一个链表的头节点 &nbsp;<code>head</code>&nbsp;，返回链表开始入环的第一个节点。&nbsp;<em>如果链表无环，则返回&nbsp;<code>null</code>。</em></p>
-
-<p>如果链表中有某个节点，可以通过连续跟踪 <code>next</code> 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 <code>pos</code> 来表示链表尾连接到链表中的位置（<strong>索引从 0 开始</strong>）。如果 <code>pos</code> 是 <code>-1</code>，则在该链表中没有环。<strong>注意：<code>pos</code> 不作为参数进行传递</strong>，仅仅是为了标识链表的实际情况。</p>
-
-<p><strong>不允许修改 </strong>链表。</p>
-
-<ul> 
-</ul>
-
-<p>&nbsp;</p>
-
-<p><strong>示例 1：</strong></p>
-
-<p><img src="https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist.png" /></p>
-
-<pre>
-<strong>输入：</strong>head = [3,2,0,-4], pos = 1
-<strong>输出：</strong>返回索引为 1 的链表节点
-<strong>解释：</strong>链表中有一个环，其尾部连接到第二个节点。
-</pre>
-
-<p><strong>示例&nbsp;2：</strong></p>
-
-<p><img alt="" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/12/07/circularlinkedlist_test2.png" /></p>
-
-<pre>
-<strong>输入：</strong>head = [1,2], pos = 0
-<strong>输出：</strong>返回索引为 0 的链表节点
-<strong>解释：</strong>链表中有一个环，其尾部连接到第一个节点。
-</pre>
-
-<p><strong>示例 3：</strong></p>
-
-<p><img alt="" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/12/07/circularlinkedlist_test3.png" /></p>
-
-<pre>
-<strong>输入：</strong>head = [1], pos = -1
-<strong>输出：</strong>返回 null
-<strong>解释：</strong>链表中没有环。
-</pre>
-
-<p>&nbsp;</p>
-
-<p><strong>提示：</strong></p>
-
-<ul> 
- <li>链表中节点的数目范围在范围 <code>[0, 10<sup>4</sup>]</code> 内</li> 
- <li><code>-10<sup>5</sup> &lt;= Node.val &lt;= 10<sup>5</sup></code></li> 
- <li><code>pos</code> 的值为 <code>-1</code> 或者链表中的一个有效索引</li> 
-</ul>
-
-<p>&nbsp;</p>
-
-<p><strong>进阶：</strong>你是否可以使用 <code>O(1)</code> 空间解决此题？</p>
-
-<details><summary><strong>Related Topics</strong></summary>哈希表 | 链表 | 双指针</details><br>
-
-<div>👍 2287, 👎 0<span style='float: right;'><span style='color: gray;'><a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a> | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a> | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a></span></span></div>
-
-
-
-
-*/
-  
-  package com.licifer.leetcode.editor.cn;
-  public class LinkedListCycleIi{
-      public static void main(String[] args) {
-           Solution solution = new LinkedListCycleIi().new Solution();
-      }
-      //leetcode submit region begin(Prohibit modification and deletion)
 /**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
+ * <p>给定一个链表的头节点 &nbsp;<code>head</code>&nbsp;，返回链表开始入环的第一个节点。&nbsp;<em>如果链表无环，则返回&nbsp;<code>null</code>。</em></p>
+ *
+ * <p>如果链表中有某个节点，可以通过连续跟踪 <code>next</code> 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 <code>pos</code> 来表示链表尾连接到链表中的位置（<strong>索引从 0 开始</strong>）。如果 <code>pos</code> 是
+ * <code>-1</code>，则在该链表中没有环。<strong>注意：<code>pos</code> 不作为参数进行传递</strong>，仅仅是为了标识链表的实际情况。</p>
+ *
+ * <p><strong>不允许修改 </strong>链表。</p>
+ *
+ * <ul>
+ * </ul>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>示例 1：</strong></p>
+ *
+ * <p><img src="https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist.png" /></p>
+ *
+ * <pre>
+ * <strong>输入：</strong>head = [3,2,0,-4], pos = 1
+ * <strong>输出：</strong>返回索引为 1 的链表节点
+ * <strong>解释：</strong>链表中有一个环，其尾部连接到第二个节点。
+ * </pre>
+ *
+ * <p><strong>示例&nbsp;2：</strong></p>
+ *
+ * <p><img alt="" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/12/07/circularlinkedlist_test2.png" /></p>
+ *
+ * <pre>
+ * <strong>输入：</strong>head = [1,2], pos = 0
+ * <strong>输出：</strong>返回索引为 0 的链表节点
+ * <strong>解释：</strong>链表中有一个环，其尾部连接到第一个节点。
+ * </pre>
+ *
+ * <p><strong>示例 3：</strong></p>
+ *
+ * <p><img alt="" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/12/07/circularlinkedlist_test3.png" /></p>
+ *
+ * <pre>
+ * <strong>输入：</strong>head = [1], pos = -1
+ * <strong>输出：</strong>返回 null
+ * <strong>解释：</strong>链表中没有环。
+ * </pre>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>提示：</strong></p>
+ *
+ * <ul>
+ * <li>链表中节点的数目范围在范围 <code>[0, 10<sup>4</sup>]</code> 内</li>
+ * <li><code>-10<sup>5</sup> &lt;= Node.val &lt;= 10<sup>5</sup></code></li>
+ * <li><code>pos</code> 的值为 <code>-1</code> 或者链表中的一个有效索引</li>
+ * </ul>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>进阶：</strong>你是否可以使用 <code>O(1)</code> 空间解决此题？</p>
+ *
+ * <details><summary><strong>Related Topics</strong></summary>哈希表 | 链表 | 双指针</details><br>
+ *
+ * <div>👍 2287, 👎 0<span style='float: right;'><span style='color: gray;'>
+ *     <a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a>
+ *     | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a>
+ *     | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a>
+ *     </span></span></div>
  */
-public class Solution {
-    //public ListNode detectCycle(ListNode head) {
-    //
-    //}
-}
+
+package com.licifer.leetcode.editor.cn;
+
+/**
+ * 解题思路：
+ * 1.快慢指针技巧，如果两个指针相遇则代表存在环，然后将其中一个指针置位空，再次相遇时即为环的起点
+ * 2.注意点：while条件判断，p1 != null && p1.next != null ,要加上p1 != null，否则while循环会报空指针
+ * 3.while条件，p1!=p2，不容易想起来
+ */
+public class LinkedListCycleIi {
+    public static void main(String[] args) {
+        Solution solution = new LinkedListCycleIi().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            ListNode p1 = head;
+            ListNode p2 = head;
+
+            if (head == null) {
+                return null;
+            }
+            while (p1 != null && p1.next != null) {
+                p1 = p1.next.next;
+                p2 = p2.next;
+                if (p1 == p2) {
+                    p2 = head;
+                    break;
+                }
+            }
+            if (p1 == null || p1.next == null) {
+                return null;
+            }
+            while (p1 != p2) {
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+            return p1;
+
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-  }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
