e9140a9b368455e8ca894213e9c657b6f8ff2e46
李思 <315068263@qq.com>
2023-09-02 11:04:59 +0800

    [344]反转字符串

---
+++ src/main/java/com/licifer/leetcode/editor/cn/ReverseString.java
@@ -0,0 +1,69 @@
+/**
+ * <p>编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 <code>s</code> 的形式给出。</p>
+ *
+ * <p>不要给另外的数组分配额外的空间，你必须<strong><a href="https://baike.baidu.com/item/原地算法" target="_blank">原地</a>修改输入数组</strong>、使用 O(1) 的额外空间解决这一问题。</p>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>示例 1：</strong></p>
+ *
+ * <pre>
+ * <strong>输入：</strong>s = ["h","e","l","l","o"]
+ * <strong>输出：</strong>["o","l","l","e","h"]
+ * </pre>
+ *
+ * <p><strong>示例 2：</strong></p>
+ *
+ * <pre>
+ * <strong>输入：</strong>s = ["H","a","n","n","a","h"]
+ * <strong>输出：</strong>["h","a","n","n","a","H"]</pre>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>提示：</strong></p>
+ *
+ * <ul>
+ * <li><code>1 &lt;= s.length &lt;= 10<sup>5</sup></code></li>
+ * <li><code>s[i]</code> 都是 <a href="https://baike.baidu.com/item/ASCII" target="_blank">ASCII</a> 码表中的可打印字符</li>
+ * </ul>
+ *
+ * <details><summary><strong>Related Topics</strong></summary>双指针 | 字符串</details><br>
+ *
+ * <div>👍 833, 👎 0<span style='float: right;'><span style='color: gray;'>
+ *     <a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a>
+ *     | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a>
+ *     | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a>
+ *     </span></span></div>
+ */
+
+package com.licifer.leetcode.editor.cn;
+
+/**
+ * 解题思路:利用左右指针交换左右元素
+ * 重点：
+ * 1.两个元素交换必须要有一个中间变量才行，不能直接左边等于右边
+ * 2.不要信定义变量，直接让左边的值放在“桌子”上就好了
+ */
+public class ReverseString {
+    public static void main(String[] args) {
+        Solution solution = new ReverseString().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void reverseString(char[] s) {
+
+            int left = 0, right = s.length - 1;
+            while (left < right) {
+                char temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;
+                right--;
+            }
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
